d010499b31d673d96dce059da2d72efd53fe4047
Ivan Eroshkin <83237781+iverosh@users.noreply.github.com>
2023-04-04 16:57:36 +0300

    edited fibonachi_method

--- LinearMin/fibonacci.py
+++ LinearMin/fibonacci.py
@@ -13,49 +13,7 @@ def count_fibs(n):
     for i in range(2, n+1):
         fibs.append(fibs[i-1] + fibs[i-2])
     return fibs
-#
-# def calc_gmp_fibonacci(a, b, t, eps, f):
-#     fibs = get_first_t_plus_2_fibonacci_numbers(t)
-#     f_calls_number = 0
-#
-#     gmp_approx = 0
-#     k = 1
-#     ak, bk = a, b
-#     lk, f_at_lk, lk_is_visited = 0, 0, False
-#     mk, f_at_mk, mk_is_visited = 0, 0, False
-#     while bk - ak > eps:
-#         # full cycle with fibs?
-#         if k == t:
-#             k = 1
-#             lk_is_visited, mk_is_visited = False, False
-#
-#         # calculate lk and (or) mk
-#         alphak = fibs[t-k]/fibs[t-k+2]
-#         if lk_is_visited:
-#             mk = alphak*ak + (1-alphak)*bk
-#             f_at_mk, f_calls_number = f(mk), f_calls_number + 1
-#         elif mk_is_visited:
-#             lk = (1-alphak)*ak + alphak*bk
-#             f_at_lk, f_calls_number = f(lk), f_calls_number + 1
-#         else:
-#             lk, mk = (1-alphak)*ak + alphak*bk, alphak*ak + (1-alphak)*bk
-#             f_at_lk, f_at_mk, f_calls_number = f(lk), f(mk), f_calls_number + 2
-#
-#         # choose next range
-#         if f_at_lk <= f_at_mk:
-#             gmp_approx = lk
-#             bk = mk
-#             mk, f_at_mk = lk, f_at_lk
-#             lk_is_visited, mk_is_visited = False, True
-#         else:
-#             gmp_approx = mk
-#             ak = lk
-#             lk, f_at_lk = mk, f_at_mk
-#             lk_is_visited, mk_is_visited = True, False
-#
-#         k += 1
-#     return gmp_approx, f_calls_number
-#
+
 
 def get_first_n_fibs(n):
     fib_numbers = [1, 1]
@@ -100,16 +58,9 @@ def fibonacci(a, b, eps, f):
                 f_calls += 1
 
     return (a_k + b_k) / 2, f_calls
-    # mu_k = lambda_k + dif
-    # f_mu_k = f(mu_k)
-    # f_calls += 1
-    # if f_mu_k == f_lambda_k:
-    #     return (lambda_k + b_k) / 2, f_calls
-    # else:
-    #     return (a_k + mu_k) / 2, f_calls
 
 
-def fibonacci1(a, b, eps, f, n):
+def fibonacci_with_given_n(a, b, f, n):  # another variant of Fibonacci method
     f_calls = 2
     # n = 4
     fibs = count_fibs(n)
