7784b84ad07a26e93d939c2d37f7a37f92d85df3
Timofey-Chibyshev <90447625+Timofey-Chibyshev@users.noreply.github.com>
2023-04-24 09:19:12 +0300

    ETA_SU$A_RABOTAET

--- 5Lab/main.py
+++ 5Lab/main.py
@@ -2,7 +2,7 @@ from ConverterLP import *
 import SimplexMethod as sm
 import numpy as np
 import task_data as td
-from scipy.optimize import linprog
+from scipy.optimize import linprog as Svoy_Mega_Super_CHESTNO_SVOY_SIMPLEX
 
 # linear constraints
 # first
@@ -22,22 +22,22 @@ min_max1 = 'min'
 '''
 x + y <= 0.5
 x - y <= 3
-10x + 0.1y >= 1
+10x + 0.1y >= -1
 '''
 
-# A2 = [[1, 1], [1, -1], [10, 0.1]]
-# b2 = [0.5, 3, 1]
-# A_signs2 = ['<=', '<=', '>=']
-# x_signs2 = [0]
-# min_max2 = 'min'
-A2 = [[-7, 1], [1, -4], [-14, -8]]
-b2 = [3, 20, 5]
+A2 = [[1, 1], [1, -1], [10, 0.1]]
+b2 = [0.5, 3, -1]
 A_signs2 = ['<=', '<=', '>=']
 x_signs2 = []
 min_max2 = 'min'
+# A2 = [[-7, 1], [1, -4], [-14, -8]]
+# b2 = [3, 20, 5]
+# A_signs2 = ['<=', '<=', '>=']
+# x_signs2 = []
+# min_max2 = 'min'
 
 # start point
-x_start = np.array([0.5, -1.5])
+x_start = np.array([0.5, -1])
 
 
 def canon_to_general(z, N1, N2):
@@ -49,38 +49,41 @@ def canon_to_general(z, N1, N2):
 
 def lp_min(x_k, A, b, A_signs, min_max, x_signs):
     c = np.append(td.calc_grad(x_k), 0)
-    # print(c)
+    print(c)
     M1, M2, N1, N2, min_max_new = manager(c, A, b, A_signs, min_max, x_signs)
     c_canon, A_canon, b_canon, A_signs_canon, x_signs_canon = common_to_canonical(c, A, b, M1, M2, N1, N2, A_signs)
-    # printing(c_canon, A_canon, b_canon, A_signs_canon, min_max_new, x_signs_canon)
+    printing(c_canon, A_canon, b_canon, A_signs_canon, min_max_new, x_signs_canon)
     z = sm.calc_global_minimum_point(c_canon, A_canon, b_canon)
-    # print('z', z)
+    print('z', z)
     x = canon_to_general(z, N1, N2)
     return x
 
 
 def conditional_gradient(x_start, A, b, A_signs, min_max, x_signs, eps):
     x_k = x_start
-    a_0 = 1/14
-    limb = 0.5  # коэффициент дробления
+    a_0 = 1/8
+    limb = 0.95  # коэффициент дробления
     iters = 0
     while np.linalg.norm(td.calc_grad(x_k), ord=2) >= eps:
         # grad = td.calc_grad(x_k)
         # print('grad(x_k1):', grad)
         # print('||grad(x_k1)||:', np.linalg.norm(grad, ord=2))
         # simplex
-        y_k = lp_min(x_k, A, b, A_signs, min_max, x_signs)
-        # A11 = np.array([[1, 1], [1, -1], [-10, -0.1], [0, -1]])
-        # b11 = np.array([0.5, 3, -1, 0])
-        # res = linprog(-td.calc_grad(x_k), A_ub=A11, b_ub=b11, bounds=(0, None))
-        # y_k = res.x
-
+        # y_k = lp_min(x_k, A, b, A_signs, min_max, x_signs)
+        # print(y_k)
+        A11 = np.array([[1, 1], [1, -1], [-10, -0.1]])
+        b11 = np.array([0.5, 3, 8])
+        x0_bounds = (None, None)
+        x1_bounds = (None, None)
+        res = Svoy_Mega_Super_CHESTNO_SVOY_SIMPLEX(td.calc_grad(x_k), A_ub=A11, b_ub=b11, bounds=[x0_bounds, x1_bounds])
+        y_k = res.x
+        # print(y_k)
         # direction
         s_k = y_k - x_k
         # eta
         n_k = np.dot(td.calc_grad(x_k), s_k)
         a_k = a_0
-        while (td.calc_func(x_k + a_k * s_k) - td.calc_func(x_k)) >= 0.5 * a_k * n_k:
+        while (td.calc_func(x_k + a_k * s_k) - td.calc_func(x_k)) > 0.5 * a_k * n_k:
             a_k = a_k * limb
         x_k = x_k + a_k * s_k
         iters += 1
